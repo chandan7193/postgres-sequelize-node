67d29243125917143f2c7cfbaa60ec902907a614
Chandan Singh <chandan.singh7193@gmail.com>
2018-01-25 15:59:28 +0530

    Delete added

--- server/routes/index.js
+++ server/routes/index.js
@@ -59,6 +59,24 @@ module.exports = (app) => {
 });
 
 
+app.delete('/api/todos/:todoId', (req,res)=>{
+  return Todo
+    .findById(req.params.todoId)
+    .then(todo => {
+      if (!todo) {
+        return res.status(400).send({
+          message: 'Todo Not Found',
+        });
+      }
+      return todo
+        .destroy()
+        .then(() => res.status(204).send())
+        .catch(error => res.status(400).send(error));
+    })
+    .catch(error => res.status(400).send(error));
+
+});
+
 
   // For any other request method on todo items, we're going to return "Method Not Allowed"
 
